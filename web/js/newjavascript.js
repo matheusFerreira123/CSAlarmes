8e3bc00cbb5ed9136cc89f7693df8313e7533c28
Gabriele Kiane <39500774+gabrielekiane@users.noreply.github.com>
2018-07-03 18:34:00 -0300

    responsivo

--- web/js/newjavascript.js
+++ web/js/newjavascript.js
@@ -2,14 +2,14 @@ function openNav(){
     
     var x = document.getElementById("barra");
     
-    if (x.className === "barra"){
+    if (x.idName === "barra"){
         
         x.className += " menujs";
         document.getElementById("threeline").innerHTML = "&Cross;";
         
     } else {
         
-        x.className = "barra";
+        x.idName = "barra";
         document.getElementById("threeline").innerHTML = "&#9776;";
     }
 }
